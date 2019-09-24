370fb727abe3abd5b56d0abff9d2692f772eb695
Bastien Saulnier <bastien.saul@gmail.com>
2019-09-24 16:26:56 +0200

    index includes

---
+++ footer.php
@@ -0,0 +1,11 @@
+</main> 
+    <footer>
+        <p>Made with <i class="fas fa-heart"></i> by <a href="https://www.wildcodeschool.com/">Wild Code School</a>.
+            Copyright 2019®</p>
+    </footer>
+    
+    <script src="https://kit.fontawesome.com/8529c819c9.js"></script>
+    <script type="text/javascript" src="burger.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Joys of the Wild</title>
+
+    <link type="text/css" media="(min-width: 961px)" rel="stylesheet" href="css/style.css" />
+    <link type="text/css" media="(max-width: 960px)" rel='stylesheet' href='css/responsive.css' />
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700&display=swap" rel="stylesheet"> 
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="description"
+        content="Joys of the Wild is a site where you can share your feelings about IT with your Wild side !" />
+    <meta name="author" content="Group 1 (The finest)">
+    <!--[if lt IE 9]>
+       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+   <![endif]-->
+    <!--[if IE 7]>
+       <link href="css/stylesie7.css" rel="stylesheet" type="text/css" media="screen">
+   <![endif]-->
+</head>
+
+<body>
+    <header>
+        <div id="logo-div">
+            <h1><a href="index.html" id="logo"><img src="img/Wild-Logo.png" alt="wild-code-school-logo">JOYS OF THE WILD</a></h1>
+        </div>
+
+        <div id="interface">
+            <div id="searchbar">
+                <button type="submit"><i class="fa fa-search"></i></button>
+                <input type="text" id="searchbox" placeholder="Search ...">
+            </div>
+
+            <div id="connect">
+                <a id="login" class="primary-btn" href="login.html">Login</a>
+                <a id="signup" class="primary-btn" href="sign-up.html">SignUp</a>
+            </div>
+        </div>
+    </header> 
