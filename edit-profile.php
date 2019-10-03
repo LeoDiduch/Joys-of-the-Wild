150967bcf6d1bd1e1b34187230f5ff129b3e9615
gregoire masson <g.masson279@gmail.com>
2019-10-03 09:52:43 +0200

    securisation formulaire edit_profile

--- edit-profile.php
+++ edit-profile.php
@@ -1,6 +1,10 @@
-<?php include 'header.php';?>
+<?php
 
-<?php include 'menus.php';?>
+require_once 'form_edit_profile.php';
+
+include 'header.php';
+
+include 'menus.php';?>
 
         <div class="container">
             <section id="user_profile">
@@ -10,13 +14,15 @@
                 </div>
 
                 <h3>Profile</h3>
-                <form method="post" action="update_user_profile.php">
+                <form method="post" action="<?=htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
                     <label for="username">Username :</label>
                     <input type="text" name="username" id="username" maxlength="30" placeholder="Team Wilders 1" />
+                    <?= $formErrors['username'] ?>
 
                     <label for="email">Email :</label>
                     <input type="email" name="email" id="email" placeholder="thebestwildersever@wcs.com" />
+                    <?= $formErrors['email'] ?>
 
                     <label for="avatar">Avatar :</label>
                     <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" class="example_f">
@@ -31,18 +37,24 @@
 
 
                 <h3>Change your password</h3>
-                <form method="post" action="edit-profile.php">
+                <form method="post" action="<?=htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
                     <label for="current_password">Current Password :</label>
                     <input type="password" name="current_password" id="current_password" maxlength="16" placeholder="Enter your current password"/>
+                    <?= $formErrors['current_password'] ?>
 
                     <label for="new_password">New Password :</label>
                     <input type="password" name="new_password" id="new_password" maxlength="16" placeholder="Enter your new password"/>
+                    <?= $formErrors['new_password'] ?>
 
                     <label for="new_password_verification">Retype New Password :</label>
                     <input type="password" id="new_password_verification" name="new_password_verification" maxlength="16" placeholder="Enter your new password again"/>
+                    <?= $formErrors['new_password_verification'] ?>
+
+                    <?= $savedChange ?>
 
                     <input type="submit" value="Save changes" class="example_f"/>
+
                 </form>
 
                 <form>
--- form_edit_profile.php
+++ form_edit_profile.php
@@ -2,6 +2,8 @@
 
 
 $formErrors = [
+    'username' => '',
+    'email' => '',
     'current_password' => '',
     'new_password' => '',
     'new_password_verification' => '',
@@ -9,15 +11,44 @@ $formErrors = [
 
 
 $savedChange = '';
+$profilChange= '';
 
 
-$validationForm = false;
+$validationPassword = false;
+$validationProfile = false;
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
+    $username = $_POST['username'];
+    $email = $_POST['email'];
     $current_password = $_POST['current_password'];
     $new_password = $_POST['new_password'];
     $new_password_verification = $_POST['new_password_verification'];
+
+    if( empty( $username ) ){
+        $formErrors['username'] = '<small>please enter your username</small>';
+    } else {
+        $formErrors['username'] = '';
+    }
+
+    switch($email){
+        case null :
+            $formErrors['email'] = '<small>please enter your email</small>';
+            break;
+        case !filter_var($email, FILTER_VALIDATE_EMAIL) :
+            $formErrors['email'] = '<small>Please enter a valid email</small>';
+            break;
+        default :
+            $formErrors['email'] = '';
+            break;
+    }
+
+    if( '' == $formErrors['username'] && '' == $formErrors['email']) {
+        $validationProfile = true;
+    }
+    if($validationProfile == true){
+        $profilChange = '<div>your new profile has been saved</div>';
+    }
 
     if( empty( $current_password ) ){
         $formErrors['current_password'] = '<small>please enter your current password</small>';
@@ -47,5 +78,3 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
 }
-
-
